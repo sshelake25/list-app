f46b4ebf3031daa32a8da3ba108ab9e8b8241145
Renuka S <renuka_somagutta@mohawkind.com>
2021-09-16 12:29:22 +0530

    service to service dependancy

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,19 +1,22 @@
 import { Component, Directive, Injectable } from '@angular/core';
+import { LoggerService } from './logger.service';
 
 @Injectable() 
 export class ConfigService {
 
-  constructor() { }
+  constructor(private loggerSrv : LoggerService) { }
 
   sayHello() {
     console.log('Hello Angular');
+    this.loggerSrv.log('sayHello: from Config service got called');
   }
 
   getTodaysDate() {
+    this.loggerSrv.log('getTodaysDate: from Config service got called');
     return new Date();
   }
 
   getLastLogintime() {
-
+    this.loggerSrv.log('getTodaysDate: from Config service got called');
   }
 }
--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoggerService {
+  logs: string[] = []; // capture logs for testing 
 
   constructor() { }
+
+  log(message: string) { //push
+    //this.logs.push('abc') // adding new element inside array 
+    this.logs.push(message);
+    console.log(message);
+  }
 }
