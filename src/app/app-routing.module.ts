94d2a4899d1b5329cf020329c484d4a306d1533b
Renuka S <renuka_somagutta@mohawkind.com>
2021-09-17 11:58:47 +0530

    more routes for application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { BlogComponent } from './blog/blog.component';
+import { ForComponent } from './for/for.component';
+import { ReactiveFormComponent } from './forms/reactive-form/reactive-form.component';
+import { TemplateFormComponent } from './forms/template-form/template-form.component';
 import { InfromationComponent } from './infromation/infromation.component';
 import { ListUserComponent } from './list-user/list-user.component';
 import { UserComponent } from './user/user.component';
@@ -12,6 +15,9 @@ const routes: Routes = [
   { path: 'info', component: InfromationComponent },
   { path: 'list', component: ListUserComponent },
   { path: 'user', component: UserComponent },
+  { path: 'reactive-forms', component: ReactiveFormComponent },
+  { path: 'template-forms', component: TemplateFormComponent },
+  { path: 'for', component: ForComponent }
 ];
 
 @NgModule({
