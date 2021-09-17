5e5a7f2a13fa777624ecb0921b58b82c42000f9b
Renuka S <renuka_somagutta@mohawkind.com>
2021-09-17 12:34:16 +0530

    nested routes implemation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { BlogComponent } from './blog/blog.component';
+import { ChildaComponent } from './childa/childa.component';
+import { ChildbComponent } from './childb/childb.component';
 import { ForComponent } from './for/for.component';
 import { ReactiveFormComponent } from './forms/reactive-form/reactive-form.component';
 import { TemplateFormComponent } from './forms/template-form/template-form.component';
 import { InfromationComponent } from './infromation/infromation.component';
 import { ListUserComponent } from './list-user/list-user.component';
+import { ParentrouteComponent } from './parentroute/parentroute.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
@@ -32,7 +35,21 @@ const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'reactive-forms', component: ReactiveFormComponent },
   { path: 'template-forms', component: TemplateFormComponent },
-  { path: 'for', component: ForComponent }
+  { path: 'for', component: ForComponent },
+
+  //this nested routes config 
+  {
+    path: 'parent',
+    component: ParentrouteComponent,
+    children: [
+      {
+        path: 'childa', component: ChildaComponent
+      },
+      {
+        path: 'childb', component: ChildbComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
